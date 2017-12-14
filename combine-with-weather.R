646aa110ae2f9fb603982f965fc00a0537635e66
viksalwan <31551949+viksalwan@users.noreply.github.com>
2017-12-14 14:26:46 -0800

    Update combine-with-weather.R

--- combine-with-weather.R
+++ combine-with-weather.R
@@ -11,19 +11,13 @@ NYC <- NYC %>%
 randomsample <- read_csv("~/Desktop/compstats/ma154-project24-teambike/final_project/randomsample.csv")
 
 
-practice<- practice %>%
-  mutate(uid=substr(starttime,1,13))
-
-# Parsing the CitiBikes Data 
-practice$starttime <- parse_date_time(practice$starttime, orders = "ymd H:M:S")
-practice$stoptime <- parse_date_time(practice$stoptime, orders = "ymd H:M:S")
 NYC$valid <- parse_date_time(NYC$valid, orders = "ymd H:M:S")
 
 # Substringing the date and the time if need be. Making a new variable month, which will help to make variables for the season. 
 NYC <- NYC %>%
   mutate(Month=month(valid)) %>%
-  mutate(date=substr(valid,1,10))
-
+  mutate(starttime=substr(valid,1,13))
+NYC$starttime <- ymd_h(NYC$starttime)
 # Making Binary Variables for the Seasons
 NYC <- NYC %>%
   mutate(summer=ifelse(Month=="6"|Month=="7"|Month=="8",1,0)) %>%
@@ -45,21 +39,17 @@ NYC <- NYC %>%
 NYC <- NYC %>%
   filter(minute(valid)=="51") 
 
-NYC <- NYC %>%
-  mutate(uid=substr(valid,1,13)) 
 
-
-# Trying to create a unique identifier after which I can join the two DF's
-
-practice$uid <- parse_date_time(practice$uid,orders = "ymd H") 
-NYC$uid <- parse_date_time(NYC$uid,orders = "ymd H")
-# So I'm joing the two df's based on the date and the hour. The uid identifier is the date and hour. 
-a <- left_join(NYC,practice,by="uid")
 
 # take a look, I was able to join the two data frames based on the date and hour. I used hourly data when the minute is 51 to maintain uniformity.  
-```
+# Cieling the Hour 
+NYC <- NYC %>%
+  mutate(valid=ceiling_date(NYC$valid, unit = "hour"))
 
-```{r}
+rs
+# Plotting the expansion of Citi Bikes Across New York
+ggplot(rs,aes(x=year,y=stations,fill=usertype))+geom_bar(stat="identity")+ylab("Number of Stations")+xlab("Station")+ggtitle("Expansion of Citi Bikes Across New York")
+
 # Parsing all the start times into one format
 mdy <- mdy_hms(randomsample$starttime) 
 ymd <- ymd_hms(randomsample$starttime) 
@@ -72,20 +62,12 @@ randomsample$starttime[is.na(randomsample$starttime)] <- f1[is.na(randomsample$s
 mdy <- mdy_hms(randomsample$stoptime) 
 ymd <- ymd_hms(randomsample$stoptime) 
 f1 <- mdy_hm(randomsample$stoptime)
-mdy[is.na(mdy)] <- ymd[is.na(mdy)] # some dates are ambiguous, here we give 
+mdy[is.na(mdy)] <- ymd[is.na(mdy)] 
 randomsample$stoptime <- mdy
 randomsample$stoptime[is.na(randomsample$stoptime)] <- f1[is.na(randomsample$stoptime)]
 
-# Cieling the Hour 
-NYC <- NYC %>%
-  mutate(valid=ceiling_date(NYC$valid, unit = "hour"))
+# Flooring the Hour
+randomsample$starttime <- floor_date(randomsample$starttime,unit = "hour")
 
-# Number of Stations Every Year
-rs <- randomsample %>%
-  mutate(year=year(starttime)) %>%
-  group_by(year,usertype) %>%
-  summarise(stations=n_distinct(start.station.id),rides=n())
-
-rs
-# Plotting the expansion of Citi Bikes Across New York
-ggplot(rs,aes(x=year,y=stations,fill=usertype))+geom_bar(stat="identity")+ylab("Number of Stations")+xlab("Station")+ggtitle("Expansion of Citi Bikes Across New York")
+# Joining the weather data and the random sample
+combined <- inner_join(NYC,randomsample,by="starttime")
